ab7ff24b23b3ff1c9cb44ae2e2da732078fcc51b
Maksim <maxred1982@gmail.com>
2020-04-04 13:48:49 +0300

    catalog

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
         /* adaptiveHeight: true, */
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        dotsClass: 'slick-dots',
         responsive: [
             {
                 breakpoint: 992,
